3efd7d1200c184a39082084a7afed4d861a66655
Nainsy Krishnatrey <137997303+nainsyk@users.noreply.github.com>
2025-03-19 16:25:48 +0530

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -5,7 +5,7 @@ import skills from "../../data/skills.json";
 import history from "../../data/history.json";
 import { getImageUrl } from "../../utils";
 
-export const Experience = () => {
+export const Skills = () => {
   return (
     <section className={styles.container} id="experience">
       <h2 className={styles.title}>Experience</h2>
